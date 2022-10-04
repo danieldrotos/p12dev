33fc8f4940fbcd183fd536738a831a730c517ab0
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2022-10-04 15:14:10 +0200

    alu2

---
+++ cpu2/alu2.v
@@ -0,0 +1,24 @@
+`include "defs.v"
+
+module alu2
+  #(
+    parameter WIDTH=32,
+    parameter IWIDTH=16
+    )
+   (
+    input wire [5:0] 	    op,
+    
+    input wire [7:0] 	    fi,
+    input wire [WIDTH-1:0]  bi,
+    input wire [WIDTH-1:0]  di,
+    input wire [IWIDTH-1:0] im,
+    
+    output wire [WIDTH-1:0] res,
+    output wire [7:0] 	    fo,
+
+    output wire 	    flag_we    
+    );
+
+   wire [WIDTH-1:0] 	    op2;
+   
+endmodule // alu2
