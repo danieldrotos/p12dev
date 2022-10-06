9278bf53b4d53673578f854021b59d43e15538a8
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2022-10-06 15:59:10 +0200

    start impl alu2

--- cpu2/alu2.v
+++ cpu2/alu2.v
@@ -20,6 +20,44 @@ module alu2
     output wire 	    flag_en
     );
 
+   // classes of inst sets
+   wire 		    op_2reg;
+   wire 		    op_2im;
+   wire 		    op_1;
+   assign op_1= op[5];
+   assign op_2im= !op_1 & op[4];
+   assign op_2reg= !op_1 & !op[4];
+
+   // second operand
    wire [WIDTH-1:0] 	    op2;
+   wire [WIDTH-1:0] 	    zex_im;
+   wire [WIDTH-1:0] 	    sex_im;
+   wire [WIDTH-1:0] 	    oex_im;
+   assign zex_im= {16'h0, im};
+   assign sex_im= {
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im[15],
+		   im
+		   };
+   assign oex_im= {16'hffff, im};
+   assign op2= op_2reg?bi:
+	       (op[3:2]=={2'b00})?zex_im:
+	       (op[3:0]=={4'b1111})?oex_im:
+	       (op[3:2]=={2'b11})?zex_im:
+	       sex_im;
    
 endmodule // alu2
