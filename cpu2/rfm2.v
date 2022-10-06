ca5d39cf9f287854f56a6296f0c1f10b2478f14a
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2022-10-06 11:55:59 +0200

    a

--- cpu2/rfm2.v
+++ cpu2/rfm2.v
@@ -1,10 +1,14 @@
 module rfm2
   #(
-    parameter WIDTH=32,
-    parameter ADDR_SIZE=4
+    parameter WIDTH=32
     )
    (
-    input wire clk
+    input wire 	     clk,
+    input wire 	     reset,
+    input wire [3:0] ra,
+    input wire [3:0] rb,
+    input wire [3:0] rd,
+    input wire [3:0] rt
     );
    
 endmodule // rfm2
