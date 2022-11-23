8a5f8a8ba4d32221fa0dbdf211cc79a3048ba0e3
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2022-11-23 10:13:00 +0100

    a

--- progs2/sim.sh
+++ progs2/sim.sh
@@ -1,3 +1,8 @@
+if [ -z "$1" ]; then
+    PROG="pmon"
+else
+    PROG="$1"
+fi
 
 xfce4-terminal -e "./tnto 5555" -H &
-./ucsim_p1516 -t2 -S uart=0,port=5555 "$1"
+./ucsim_p1516 -t2 -S uart=0,port=5555 "$PROG"
