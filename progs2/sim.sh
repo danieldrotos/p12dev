5b9bb3b69802885ee2e3adaf5af14922842a7be8
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2023-04-22 12:24:05 +0200

    aa

--- progs2/sim.sh
+++ progs2/sim.sh
@@ -7,4 +7,4 @@ fi
 xfce4-terminal -e "./tnto 5555" -H &
 I="-I if=rom[0xffff]"
 #I=""
-./ucsim_p1516 -t2 -S uart=0,port=5555 $I -e "uart0_check_often=1" "$PROG"
+./ucsim_p1516 -t2 -S uart=0,port=5555 $I -g -e "uart0_check_often=1" "$PROG"
