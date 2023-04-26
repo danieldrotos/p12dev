eab3f8034c386d5cf77fa2e5f32b5bbc405d8242
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2023-04-26 08:58:50 +0200

    args

--- sim.bat
+++ sim.bat
@@ -1,4 +1,6 @@
 start "UART terminal" "%ProgramFiles(x86)%\teraterm\ttermpro.exe" /T=1 telnet://localhost:5555
 rem start "p2223 UART0" powershell -noexit -command "./tnto.bat"
 
-ucsim_p1516 -t2 -g -S uart=0,port=5555 -I if=rom[0xffff] -e "uart0_check_often=1" pmon
+set arg1=%1
+
+ucsim_p1516 -t2 -g -S uart=0,port=5555 -I if=rom[0xffff] -e "uart0_check_often=1" progs2/pmon %arg1%
--- sim.sh
+++ sim.sh
@@ -1,10 +1,5 @@
-if [ -z "$1" ]; then
-    PROG="pmon"
-else
-    PROG="$1"
-fi
 
 xfce4-terminal -e "./tnto 5555" -H &
 I="-I if=rom[0xffff]"
 #I=""
-./ucsim_p1516 -t2 -S uart=0,port=5555 $I -g -e "uart0_check_often=1" "$PROG"
+./ucsim_p1516 -t2 -S uart=0,port=5555 $I -g -e "uart0_check_often=1" progs2/pmon $1
