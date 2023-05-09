4401b257804c2d519339507fe19bd796d701c1f7
Drotos Daniel <drdani@mazsola.iit.uni-miskolc.hu>
2023-05-09 19:52:53 +0200

    add new ff prog

--- progs2/ff.asm
+++ progs2/ff.asm
@@ -6,145 +6,130 @@ pvhex	= 0xf011
 
 porta	= 0xff00
 portb	= 0xff01
-led		= 0xff01
-sw		= 0xff10
-btn		= 0xff20
+led	= 0xff01
+sw	= 0xff10
+btn	= 0xff20
 
-pre		= 0xff51
+pre	= 0xff51
 clock	= 0xff50
 ffcnt	= 0xff52
 
-		org		1
-		ldl0	sp,verem
+	org	1
+	
+	ldl0	sp,verem
 
-;		ldl0	r0,hello_string
-;		call	print
+;	ldl0	r0,hello_string
+;	call	print
 
-		ldl0	r0,25000
-		st		r0,pre
-		ldl0	r0,100
-		st		r0,ffcnt
-		ldl0	r0,1
-		st		r0,led
+	ldl0	r0,25000
+	st	r0,pre
+	ldl0	r0,100
+	st	r0,ffcnt
+	ldl0	r0,1
+	st	r0,led
 		
-		ld		r0,btn
-		st		r0,last_btn
+	ld	r0,btn
+	st	r0,last_btn
 ciklus:
-		call	monitor
+	call	monitor
 		
-		ld		r0,ffcnt
-		sz		r0
-		Z call	ff_leptetes
+	ld	r0,ffcnt
+	sz	r0
+	Z call	ff_leptetes
 		
-		ldl0	r0,2
-		call	pressed
-		NC jmp 	tovabb
+	ldl0	r0,2
+	call	pressed
+	NC jmp 	tovabb
 ; BTN1 megnyomodott
 ; run= !run
-		ld		r0,run
-		sz		r0
-		Z ldl0	r0,1
-		NZ ldl0	r0,0
-		st		r0,run
-		;add		r0,'0'
-		;call	putchar
-		;jmp	tovabb
+	ld	r0,run
+	sz	r0
+	Z ldl0	r0,1
+	NZ ldl0	r0,0
+	st	r0,run
 		
 tovabb:
-		;ld		r0,btn
-		;ldl0	r1,4
-		;call	pvhex
-		;ldl0	r0,10
-		;call	putchar
-		
-		jmp		ciklus
+	jmp	ciklus
 
 ido_tabla:
-		dd		10
-		dd		20
-		dd		50
-		dd		100
-		dd		200
-		dd		500
-		dd		750
-		dd		1000
+	dd	10
+	dd	20
+	dd	50
+	dd	100
+	dd	200
+	dd	500
+	dd	750
+	dd	1000
 		
 ff_leptetes:
-		push	lr
-		ld		r0,run
-		sz		r0
-		Z jmp nem_leptet
-		ld		r0,sw
-		btst	r0,7
-		ld		r1,r0,ido_tabla
-		st		r1,ffcnt
-		
-		ld		r0,led
-		test	r0,0x8000
-		NZ ldl0	r0,1
-		Z shl	r0
-		st		r0,led
+	push	lr
+	ld	r0,run
+	sz	r0
+	Z jmp	nem_leptet
+	ld	r0,sw
+	btst	r0,7
+	ld	r1,r0,ido_tabla
+	st	r1,ffcnt
+	
+	ld	r0,led
+	test	r0,0x8000
+	NZ ldl0	r0,1
+	Z shl	r0
+	st	r0,led
 nem_leptet:
-		pop		lr
-		ret
+	pop	lr
+	ret
 
 ; R0: address of string 
 print:
-		push	lr
-		sz		r0
-		Z jmp	print_vege
-		mov		r1,r0
-		ldl0	r2,0
+	push	lr
+	sz	r0
+	Z jmp	print_vege
+	mov	r1,r0
+	ldl0	r2,0
 print_ciklus:
-		ld		r0,r1+,r2
-		sz		r0
-		Z jmp	print_vege
-		call	putchar
-		jmp		print_ciklus
+	ld	r0,r1+,r2
+	sz	r0
+	Z jmp	print_vege
+	call	putchar
+	jmp	print_ciklus
 print_vege:		
-		pop		lr
-		ret
-		
+	pop	lr
+	ret
+	
 ; R0: bit mask of examined BTN
 pressed:
-		push	lr
-		ld		r1,last_btn
-		ld		r2,btn
-		and		r1,r0
-		and		r2,r0
-		cmp		r1,r2
-		EQ jmp	pressed_hamis
-		ld		r1,last_btn
-		mov		r3,r0
-		not		r3
-		and		r1,r3
-		or		r1,r2
-		st		r1,last_btn
-		
-		;mov		r0,r1
-		;ldl0	r1,4
-		;call	pvhex
-		;ldl0	r0,10
-		;call	putchar
-		
-		sz		r2
-		Z jmp	pressed_hamis
+	push	lr
+	ld	r1,last_btn
+	ld	r2,btn
+	and	r1,r0
+	and	r2,r0
+	cmp	r1,r2
+	EQ jmp	pressed_hamis
+	ld	r1,last_btn
+	mov	r3,r0
+	not	r3
+	and	r1,r3
+	or	r1,r2
+	st	r1,last_btn	
+	sz	r2
+	Z jmp	pressed_hamis
 pressed_igaz:	
-		sec
+	sec
 	jmp	pressed_vege
 
 pressed_hamis:
 	clc
 pressed_vege:
-		pop		lr
-		ret
+	pop	lr
+	ret
 		
 hello_string:
-		db		"Hello World!\n"
+	db	"Hello World!\n"
 last_btn:
-		ds		1
+	ds	1
 run:
-		db		1
+	db	1
 		
-		ds		100
+	ds	100
 verem:
