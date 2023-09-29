68c002cfbd0a9663821279e82f949f940aa2f4f2
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2023-09-29 11:28:03 +0200

    fixes

--- tools/sim.bat
+++ tools/sim.bat
@@ -1,6 +1,9 @@
-start "UART terminal" "%ProgramFiles(x86)%\teraterm\ttermpro.exe" /T=1 telnet://localhost:5555
+start "UART" "%ProgramFiles(x86)%\teraterm\ttermpro.exe" /T=1 telnet://localhost:5555
 rem start "p2223 UART0" powershell -noexit -command "./tnto.bat"
+
+start "cmd1" "%ProgramFiles(x86)%\teraterm\ttermpro.exe" /T=1 telnet://localhost:6666
+start "cmd2" "%ProgramFiles(x86)%\teraterm\ttermpro.exe" /T=1 telnet://localhost:6666
 
 set arg1=%1
 
-ucsim_p1516 -t2 -g -S uart=0,port=5555 -I if=rom[0xffff] -e "uart0_check_often=1" progs2/pmon %arg1%
+ucsim_p1516 -t2 -Z6666 -S uart=0,port=5555 -I if=rom[0xffff] -g -e "uart0_check_often=1" progs2/pmon %arg1%
--- tools/sim.sh
+++ tools/sim.sh
@@ -6,6 +6,6 @@ I="-I if=rom[0xffff]"
 xfce4-terminal -T "cmd1" -e "./tnto 6666" -H &
 xfce4-terminal -T "cmd2" -e "./tnto 6666" -H &
 
-./ucsim_p1516 -t2 -Ccfg.cmd -Z6666 -S uart=0,port=5555 $I -g -e "uart0_check_often=1" progs2/pmon $1
+./ucsim_p1516 -t2 -Z6666 -S uart=0,port=5555 $I -g -e "uart0_check_often=1" progs2/pmon $1
 #reset
 
