e9d8f5c7317d98816882df42c27fdd99422f1aec
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2023-11-13 12:12:53 +0100

    le

--- progs2/fn_lined.asm
+++ progs2/fn_lined.asm
@@ -28,6 +28,8 @@ le_start:
 	st	r2,le_cursor_pos
 	ld	r1,le_buf_addr	; buf[0]= 0
 	st	r2,r1
+	mvzl	r1,le_ptr	; ptr= 0
+	st	r2,r1
 	pop	r2
 	pop	r1
 	pop	lr
@@ -40,10 +42,32 @@ tu_fgets:
 	.global	le_read
 	.global	tu_fgets
 	push	lr
-
-	call	uart_check
-	NC jmp	ler_ret
-	call	uart_read
+	push	r0
+	push	r1
+	push	r2
+	push	r3
+	
+	call	uart_check	; if there is no char
+	NC jmp	ler_ret		; return with false
+	call	uart_read	; read one char
+	cmp	r0,13		; check CR and LF
+	jz	ler_true	; both accepted as ENTER
+	cmp	r0,10
+	jz	ler_true
+	;; not ENTER
+	cmp	r0,8		; is it backspace?
+	jnz	ler_nobs
+	;; process Backspace
+	jmp	ler_false
+	
+ler_nobs:	
+	cmp	r0,32		; skip ctrl chars
+	ULT jmp	ler_false
+	cmp	r0,128		; skip graphic chars
+	UGE jmp	ler_false
+	;; process accpeted char
+	ld	r1,le_buf_addr
+	ld	r2,le_ptr
 	
 ler_false:
 	clc
@@ -51,7 +75,11 @@ ler_false:
 ler_true:
 	sec
 	jmp	ler_ret
-ler_ret:	
+ler_ret:
+	pop	r3
+	pop	r2
+	pop	r1
+	pop	r0
 	pop	lr
 	ret
 
@@ -61,6 +89,7 @@ le_buf_addr:
 	db	0
 le_cursor_pos:
 	db	0
-	
+le_ptr:
+	db	0
 	.ends
 	
