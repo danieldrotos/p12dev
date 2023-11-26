e57c33265aa2eb348fb062552e4521d49b87b7a1
Daniel Drotos <daniel.drotos@uni-miskolc.hu>
2023-11-26 21:12:55 +0100

    more comments

--- examples/blink_fr2.asm
+++ examples/blink_fr2.asm
@@ -22,14 +22,17 @@ main:
 cycle:
 	call	blink		; handle clock events
 	call	user_input	; handle user input
-	call	btn_press	; handle press on BTN[1]
+	call	btn_press	; handle press of BTN[1]
 	jmp	cycle
 
-	;; variable
+	
+	;; variables
 frequ:	.db	5		; frequency [Hz]
 ptime:	.db	0		; period time [ms]
 buffer:	.ds	40		; user input buffer
+
 	
+	;; Calculate period time for given frequency
 	;; In : frequ variable in [Hz]
 	;; Out: ptime variable in [ms]
 	;; ptime= 1000/frequ
@@ -42,38 +45,44 @@ fr2pt:
 	pop	lr
 	ret
 
+	
+	;; Blink LED when timer expires
 blink:
 	push	lr
 	;; check clock
-	ld	r0,CLOCK.BCNT2
-	sz	r0
-	NZ jmp	blink_ret
+	ld	r0,CLOCK.BCNT2	; get back counter
+	sz	r0		; check if zero
+	NZ jmp	blink_ret	; if not, go out
 do_blink:	
 	;; expired: blink on LED[0]
-	ld	r0,GPIO.LED
-	xor	r0,1
-	st	r0,GPIO.LED
+	ld	r0,GPIO.LED	; read all LEDs
+	xor	r0,1		; negate [0]
+	st	r0,GPIO.LED	; show it
 	;; restart clock
-	ld	r0,ptime
-	st	r0,CLOCK.BCNT2
+	ld	r0,ptime	; use calculated period time
+	st	r0,CLOCK.BCNT2	; restart back counter
 blink_ret:	
 	pop	lr
 	ret
 
+
+	;; Check for user input and handle it
 cmd_quit:
-	.db	"quit"
-	
+	.db	"quit"		; special command to return to monitor
+
 user_input:
 	push	lr
-	call	le_read
-	NC jmp	ui_ret
+	call	le_read		; read line
+	NC jmp	ui_ret		; if not ready, go out
 process_input:	
 	mvzl	r0,10		; echo ENTER
 	call	putchar
 	mvzl	r0,buffer	; check for word "quit"
 	mvzl	r1,cmd_quit
-	call	_strieq
-	C call	monitor
+	call	_strieq		; case insensitive compare
+	C call	monitor		; if eq, go out to monitor
+	;; it was not QUIT command,
+	;; suppose it is a decimal number
 	mvzl	r0,buffer	; convert entered decimal number
 	call	_dtoi		; to integer value
 	cmp	r1,1		; check low limit
@@ -84,11 +93,11 @@ process_input:
 ui_good:	
 	st	r1,frequ	; store as frequ
 	call	fr2pt		; calculate new period time
-	mvzl	r0,0		; force blink event
+	mvzl	r0,0		; force blink event by expiring counter
 	st	r0,CLOCK.BCNT2	; to pick up new value
 	jmp	ui_done
 ui_wrong:
-	call	_pesf
+	call	_pesf		; print error message
 	.db	"%d not in range 1-20\n"
 ui_done:	
 	call	prompt		; print prompt
@@ -97,6 +106,8 @@ ui_ret:
 	pop	lr
 	ret
 
+
+	;; Print prompt
 prompt:
 	push	lr
 	call	_pes
@@ -104,14 +115,18 @@ prompt:
 	pop	lr
 	ret
 
+
+	;; Check for button press and handle it
 btn_press:
 	push	lr
 	mvzl	r0,2		; mask of BTN[1]
-	call	pressed
-	C call	monitor
+	call	pressed		; if pressed
+	C call	monitor		; then go out to monitor
 	pop	lr
 	ret
-	
+
+
+	;; STACK
 	.ds	99
 stack:	.ds	1
 
