c757c9cdb7ae0125b6a8d9b9e227a2ee57eca059
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2025-01-19 16:16:09 +0100

    tu_getc tester

---
+++ sw/progs2/tutest.asm
@@ -0,0 +1,29 @@
+	.cpu	p2
+
+	.org	1
+	mvzl	sp,stack
+	jmp	main
+
+str:	.db	"s\e[Ae"
+main:
+	mvzl	r10,str
+	mvzl	r9,0
+cyc:
+	ld	r4,r9+,r10
+	sz	r4
+	jz	fin
+	mov	r1,r4
+	call	ti_process_char
+	mov	r2,r4
+	getf	r3
+	btst	r3,2
+	shr	r3
+	call	eprintf
+	.db	"str=%d R4=%d F.C=%d\n"
+	jmp	cyc
+fin:	
+	call	monitor
+	
+stack:	.ds	1
+	.ds	100
+	
